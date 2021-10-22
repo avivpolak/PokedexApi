4cbeaefaf9b469b2bf75d30c9f422cd21e7d9ca5
AVIV POLAK <avivp0437@gmail.com>
2021-10-22 17:45:30 +0300

    error handler

--- src/back/server.js
+++ src/back/server.js
@@ -1,3 +1,5 @@
+/** @format */
+
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -6,56 +8,59 @@ const P = new Pokedex();
 
 //const router = require("./PokemonRouter");
 
-// start the server
-app.listen(port, function () {
-    console.log("app started");
-});
-
 // route our app
 app.get("/", function (req, res) {
-    res.send("hello world!");
+  res.send("hello world!");
 });
 
 async function getPokemonByNameFromAPI(name) {
-    return await P.getPokemonByName(name)
-        .then(function (response) {
-            let info = {
-                name: response.name,
-                height: response.height,
-                weight: response.weight,
-                types: Gettypes(response.types),
-                front_pic: response.sprites.front_default,
-                back_pic: response.sprites.back_default,
-                abilities: GetAbilities(response.abilities),
-            };
-            return info;
-        })
-        .catch(function (error) {
-            throw new Error("There was an ERROR: ", error);
-        });
+  return await P.getPokemonByName(name)
+    .then(function (response) {
+      let info = {
+        name: response.name,
+        height: response.height,
+        weight: response.weight,
+        types: Gettypes(response.types),
+        front_pic: response.sprites.front_default,
+        back_pic: response.sprites.back_default,
+        abilities: GetAbilities(response.abilities),
+      };
+      return info;
+    })
+    .catch(function (error) {
+      throw new Error("There was an ERROR: ", error);
+    });
 }
 function Gettypes(types) {
-    let rTypes = [];
-    for (let obj of types) {
-        rTypes.push(obj.type.name);
-    }
-    return rTypes;
+  let rTypes = [];
+  for (let obj of types) {
+    rTypes.push(obj.type.name);
+  }
+  return rTypes;
 }
 function GetAbilities(abilities) {
-    let rAbilities = [];
-    for (let obj of abilities) {
-        rAbilities.push(obj.ability.name);
-    }
-    return rAbilities;
+  let rAbilities = [];
+  for (let obj of abilities) {
+    rAbilities.push(obj.ability.name);
+  }
+  return rAbilities;
 }
 app.get("/pokemon/get/:id", async (req, res) => {
-    let info = getPokemonByNameFromAPI(parseInt(req.params.id));
-    info.then((result) => {
-        console.log(result);
-        res.send(result);
-    });
-    // console.log(info);
-    // res.send(info);
+  let info = getPokemonByNameFromAPI(parseInt(req.params.id));
+  info.then((result) => {
+    res.send(result);
+  });
+  info.catch((err) => {
+    res.status(404);
+    res.send("404");
+  });
+
+  // console.log(info);
+  // res.send(info);
+});
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 //app.get("/pokemon/get", router);
@@ -93,3 +98,7 @@ app.get("/pokemon/get/:id", async (req, res) => {
 //     }
 //     return rAbilities;
 // }
+// start the server
+app.listen(port, function () {
+  console.log("app started");
+});
